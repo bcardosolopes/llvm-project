d90c98a3761d2120b0a7ae77ea587b37f104bac1
Bruno Cardoso Lopes <bruno.cardoso@gmail.com>
2026-02-09 22:46:42 -0800

    Fix builtin-abort.c and call-extra-attrs.cpp: update call-site nothrow syntax
    Upstream uses `nothrow` keyword directly on cir.call instead of extra() attr.

--- clang/test/CIR/IncubatorTests/CodeGen/call-extra-attrs.cpp
+++ clang/test/CIR/IncubatorTests/CodeGen/call-extra-attrs.cpp
@@ -20,9 +20,9 @@ int s2(int a, int b) {
 
 // CIR: cir.func{{.*}} no_inline optnone {{.*}} @_Z2s0ii(%{{.*}}, %{{.*}}) -> {{.*}} extra(#cir<extra({nothrow = #cir.nothrow})>)
 // CIR: cir.func{{.*}} no_inline optnone {{.*}} @_Z2s1ii(%{{.*}}, %{{.*}}) -> {{.*}} extra(#cir<extra({nothrow = #cir.nothrow})>)
-// CIR: cir.call @_Z2s0ii(%{{.*}}, %{{.*}}) : ({{.*}}, {{.*}}) -> {{.*}} extra(#cir<extra({nothrow = #cir.nothrow})>)
+// CIR: cir.call @_Z2s0ii(%{{.*}}, %{{.*}}) nothrow : ({{.*}}, {{.*}}) -> {{.*}}
 // CIR: cir.func {{.*}} optnone {{.*}} @_Z2s2ii(%{{.*}}, %{{.*}}) -> {{.*}}
-// CIR-NOT: cir.call @_Z2s1ii(%{{.*}}, %{{.*}}) : ({{.*}}, {{.*}}) -> {{.*}} extra(#cir<extra({nothrow
+// CIR-NOT: cir.call @_Z2s1ii(%{{.*}}, %{{.*}}) : ({{.*}}, {{.*}}) -> {{.*}} nothrow
 
 // LLVM: define dso_local i32 @_Z2s0ii(i32 %0, i32 %1) #[[#ATTR1:]]
 // LLVM: define dso_local i32 @_Z2s1ii(i32 %0, i32 %1) #[[#ATTR1:]]
