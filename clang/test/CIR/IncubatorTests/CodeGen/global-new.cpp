c9b79405e1ae6c94f1ee695f2426f98db29d8abb
Bruno Cardoso Lopes <bruno.cardoso@gmail.com>
2026-02-09 22:33:51 -0800

    [CIR][IncubatorTests] Fix CHECK lines for globals.c and global-new.cpp
    globals.c: Reorder CHECK lines for foo() function to come after all global
    CHECK lines since upstream emits all globals before functions (matching OG).
    
    global-new.cpp: Update LLVM global_ctors CHECK to match upstream/OG behavior
    of using a single _GLOBAL__sub_I entry instead of individual entries.
    
    Note: global-new.cpp still fails on CIR_EH checks due to NYI
    emitCXXNewExpr operator delete with exceptions.

--- clang/test/CIR/IncubatorTests/CodeGen/global-new.cpp
+++ clang/test/CIR/IncubatorTests/CodeGen/global-new.cpp
@@ -68,7 +68,7 @@ e *g = new e(0);
 // LLVM_EH:   ret void
 // LLVM_EH: }
 
-// LLVM-DAG: @llvm.global_ctors = appending constant [2 x { i32, ptr, ptr }] [{ i32, ptr, ptr } { i32 65535, ptr @__cxx_global_var_init, ptr null }, { i32, ptr, ptr } { i32 65535, ptr @__cxx_global_var_init.1, ptr null }]
+// LLVM-DAG: @llvm.global_ctors = appending global [1 x { i32, ptr, ptr }] [{ i32, ptr, ptr } { i32 65535, ptr @_GLOBAL__sub_I_global_new.cpp, ptr null }]
 // LLVM: define internal void @__cxx_global_var_init()
 // LLVM: call ptr @_Znwm(i64 1)
 
